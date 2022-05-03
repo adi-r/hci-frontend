74aabbc50e582d8e4ca5084cd99c0d4d1de0671a
Aditya Ranganathan <ranga.aditya97@gmail.com>
2022-05-03 01:28:35 -0500

    shit2

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,27 +5,38 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-            <div>
+            <div style={{ fontSize: "24px " }}>
+                <br />
+                <h2>  Home </h2>
+                <br />
+                <div></div>
 
-            Group 8 CS 522
-            FORMATIVE EVALUATION
-            FORAGER: Handling food wastage through the community
-            INTRODUCTION
-            Our project leverages a community-driven approach to handle food wastage by donating excess
-            food which is then received by other users in the application who may be food insecure or just
-            lack a certain ingredient. The goal of our project is to develop an application that not only raises
-            awareness about food wastage but also facilitates the sharing of food between users thereby
-            cutting down on the potential wastage. This not only brings the community closer but also allows
-            people to meaningfully contribute to NGOs and food-insecure people.
+                <br />
+
+                <div style={{ textAlign: "center" }}>  <b> Formative Evaluation Steps </b> </div>
+
+                <br />
+
+                <ul style={{ textAlign: "left" }}>
+                    <li>
+                        Before the users were asked to perform the tasks, they were given a brief introduction of
+            the system setup and interactions supported by our systems by a facilitator.
+                    </li>
+                    <li>
+                        Users were asked to perform 3 tasks
+                    </li>
+                    <li>
+                        One member of the team observed the users while performing tasks for observations.
+                    </li>
+                    
+                </ul>
+
+                <br />
+            
             EVALUATION METHOD
             USERS
             The study was conducted using “Wizard of Oz”, “Think Aloud” methodologies, and paper
             prototypes on 3 users. All 3 users are from CS 522
-            FORMATIVE EVALUATION STEPS
-            1. Before the users were asked to perform the tasks, they were given a brief introduction of
-            the system setup and interactions supported by our systems by a facilitator.
-            2. Users were asked to perform 3 tasks
-            3. One member of the team observed the users while performing tasks for observations.
             TASKS
             1. TASK CARDS
             Task 1: Add a new listing for donation/food item
@@ -223,3 +234,66 @@ export default class About extends Component {
         )
     }
 }
+
+
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div style={{ fontSize: "24px " }}>
+                <br />
+                <h2>  Home </h2>
+                <br />
+                <div></div>
+
+                <br />
+
+                <div style={{ textAlign: "center" }}>  <b> Project Goals </b> </div>
+
+                <br />
+
+                <ul style={{ textAlign: "left" }}>
+                    <li>According to the EPA and USDA, the United States discards more food than any other country in the world: 80 billion pounds every year. 
+                        That’s estimated to be 30-40 percent of the entire US food supply and equates to 219 pounds of waste per person.
+                    </li>
+                    <li>Our project aims to solve this issue at the grassroots level by allowing users in a community to share any unnecessary food with other members of the community. 
+                        This not only cuts down on food waste but also creates public awareness about the matter.
+                    </li>
+                    <li>We propose our solution: FORAGER, an interface that allows users to not only donate unnecessary food but also receives food items as and when needed. 
+                        Currently built on Android, our mobile application allows users to choose what items they wish to donate by adding the corresponding details on the donation form while users can choose to receive food items by accepting donations and going to the location to pick up the item. 
+                    </li>
+                    
+                </ul>
+
+                <br />
+                
+                <div style={{ textAlign: "center", }}> <b> TARGET USERS </b> </div>
+
+                <ul style={{ textAlign: "left" }}>
+                    <li>Our primary users are users with smartphones who wish to donate and/or receive excess food.</li>
+                    <li>Teens (13 - 18) and Adults (18 - 65 years old) are the primary targets for this project.</li>
+                    <li>Users should be familiar with modern smartphones and social media applications as well as the methods to operate a camera.</li>
+
+
+                </ul>
+
+                <br/>
+
+                <div style={{ textAlign: "center", }}> <b> ABOUT US </b> </div>
+
+                <ul style={{ textAlign: "left" }}>
+                    <li>Aditya Ranganathan, CS Grad</li>
+                    <li>Karthik Singh, CS Grad</li>
+
+
+                </ul>
+            </div>
+               
+
+        )
+    }
+}
+
+
+
